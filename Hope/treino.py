a6b6a6afe471e6bc6f34284c3f0abacb4815a0e7
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-01 01:05:43 -0300

    Update treino.py

--- Hope/treino.py
+++ Hope/treino.py
@@ -9,3 +9,14 @@ import tensorflow
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer
+intents = json.load(open('intents.json').read())
+words = ["o"]
+clases = ["o"]
+documents = ["o"]
+ignore_letters = ["?", "!", ".", ",", ";", "'"]
+
+for intents in intents['intents']:
+    for pattern in intent['pattern']:
+        word_list = nltk.tokenize(pattern)
