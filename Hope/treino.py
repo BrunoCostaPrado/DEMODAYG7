fca7bb550bdf729d460b8555f736b57154391529
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-01 16:15:55 -0300

    ;

--- Hope/treino.py
+++ Hope/treino.py
@@ -1,18 +1,18 @@
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.models import Sequential
+from nltk.stem import WordNetLemmatizer
 import random
 import json
 import pickle
 import numpy as np
 import nltk
-from nltk.stem import WordNetLemmatizer
-import tensorflow
+nltk.download('punkt')
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer
 
-intents = json.loads(open('intents.json').read())
+intents = json.loads(open('Hope/intents.json').read())
 
 words = []
 classes = []
@@ -20,11 +20,11 @@ documents = []
 ignore_letters = ['?', '!', '.', ',', ';']
 
 for intent in intents['intents']:
-    for pattern in intent['pattern']:
+    for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
         words.append(word_list)
         documents.append((word_list, intent['tag']))
-        if intents['tag'] not in classes:
+        if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
             print(documents)
