a7ad67bde064ab1cd90bcdae63c59ac869b7b378
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-03 16:51:42 -0300

    Update Chance.sql

--- banco_de_dados/Chance.sql
+++ banco_de_dados/Chance.sql
@@ -6,4 +6,4 @@ create table if not exists cliente(cpf integer primary key , nome_completo varch
 create table if not exists funcionario(cpf integer primary key , nome_completo varchar(100), email varchar(100), cidade varchar(100), estado_civil varchar(100), data_nasc integer, cep integer, telefone integer, estado varchar(100), genero varchar(100), pis integer, RG integer, endereco varchar(100), CTPS varchar(100));
 create table if not exists servico(tipo varchar(100), datas integer, quantidade integer);
 create table if not exists pagamento(id_pagamento integer primary key);
-select * from funcionario;
+
