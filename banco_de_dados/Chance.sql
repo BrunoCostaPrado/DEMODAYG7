f4446c96dcbf933e83ed7b7a3b686afdfe2a15db
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-03 15:22:33 -0300

    .

---
+++ banco_de_dados/Chance.sql
@@ -0,0 +1,4 @@
+create database if not exists Chance;
+create table if not exists autonomo (cpf integer primary key , nome varchar(100), sobrenome varchar(100),);
+create table if not exists cliente;
+create table if not exists funcionario;
