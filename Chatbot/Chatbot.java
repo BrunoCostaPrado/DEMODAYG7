c0311d6adcb92a196c7f4552bc532fd5422927dd
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-05 10:40:38 -0300

    .

--- Chatbot/Chatbot.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void public static void main(String[] args) {
-        Spark.staticFileLocation( "public_html");
-        
-        Spark.get ("/", (request, response)->{
-            return "hello World";
-        } ) 
-    }
-}
