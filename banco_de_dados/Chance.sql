e1e0c178e7fe02eb9d9898cd8fcbd60db58c5756
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-14 17:47:58 -0300

    Update Chance.sql

--- banco_de_dados/Chance.sql
+++ banco_de_dados/Chance.sql
@@ -1,14 +1,6 @@
 drop database chance;
 create database if not exists chance;
 use chance;
-#create table if not exists autonomo(cpf integer primary key, nome_completo varchar(100), email varchar(100), empresa varchar(100), cidade varchar(100), profissao varchar(100), estado_civil varchar(100), data_nasc integer, cep integer, telefone integer, estado varchar(100), gender varchar(100), endereco varchar(100));
-#create table if not exists cliente(cpf integer primary key , nome_completo varchar(100), email varchar(100), cidade varchar(100), estado_civil varchar(100), data_nasc integer, cep integer, telefone integer, estado varchar(100), genero varchar(100), endereco varchar(100));
-#create table if not exists funcionario(cpf integer primary key , nome_completo varchar(100), email varchar(100), cidade varchar(100), estado_civil varchar(100), data_nasc integer, cep integer, telefone integer, estado varchar(100), genero varchar(100), pis integer, RG integer, endereco varchar(100), CTPS varchar(100));
-#create table if not exists servico(tipo varchar(100), datas integer, quantidade integer);
-#create table if not exists pagamento(id_pagamento integer primary key, pix varchar(80),credito varchar(80),debito varchar(80),tranferencia varchar(150),boleto varchar(100));
-#show tables
-
-
 
 CREATE TABLE CadastoAutonomo (
 Nome VARCHAR(80),
@@ -24,7 +16,7 @@ CNPJ INTEGER,
 CEP INTEGER,
 DataNasc INTEGER,
 Estado_Civil VARCHAR(20),
-PRIMARY KEY(CPF,CNPJ)
+PRIMARY KEY(CPF)
 );
 
 CREATE TABLE CadastroCliente (
@@ -100,16 +92,21 @@ CREATE TABLE Prove (
 );
 
 CREATE TABLE Pagar (
-    IDPagamento integer,
+    IDPagamento INTEGER,
     FOREIGN KEY (IDPagamento)
         REFERENCES Pagamento (IDPagamento),
-        CPF INTEGER,
-    CNPJ INTEGER,
-    FOREIGN KEY (CNPJ)
-        REFERENCES CadastoAutonomo (CNPJ,CPF)
+    CPF INTEGER,
+    FOREIGN KEY (CPF)
+        REFERENCES CadastoAutonomo (CPF)
 );
 
 
 CREATE TABLE Prover (
-    IdServiço INTEGER, CPF INTEGER, CNPJ INTEGER, FOREIGN KEY (IdServiço) REFERENCES Serviço (IdServiço), FOREIGN KEY (CNPJ) REFERENCES CadastoAutonomo (CPF , CNPJ));
+    IdServiço INTEGER,
+    CPF INTEGER,
+    FOREIGN KEY (IdServiço)
+        REFERENCES Serviço (IdServiço),
+    FOREIGN KEY (CPF)
+        REFERENCES CadastoAutonomo (CPF)
+);
 show tables;
